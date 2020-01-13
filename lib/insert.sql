a6cc76c7cd06a47cfcdcc1eab149d069f8b9c3bb
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-01-13 19:20:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,14 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "The Expanse", 1, 1), (2, "The Dark Elf Trilogy", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
-INSERT INTO subgenres (id, name) VALUES (1, "space opera"), (2, "epic fantasy");
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
-INSERT INTO authors (id, name) VALUES (1, "James S. A. Corey"), (2, "R. A. Salvatore");
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Leviathan Wakes", 2011, 1), (2, "Caliban's War", 2012, 1), (3, "Abaddon's Gate", 2013, 1), (4, "Homeland", 1990, 2), (5, "Exile", 1990, 2), (6, "Sojourn", 1991, 2);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "James Holden", "Let's get this clear", "belterlowda", 1, 1), (2, "Amos Burton", "I'll crush you", "human", 1, 1), (3, "Krisjen Avarasala", "Together we can meet this threat", "human", 1, 1), (4, "Naomi Nagata", "I am Belterlowda", "Belterlowda", 1, 1);
-
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Drizzt D'Urden", "motto one", "dark elf", 2, 2), (6, "Zaknafein", "enter the zone", "dark elf", 2, 2), (7, "belwar", "by mithrill", "dwarf", 2, 2), (8, "gwynyvar", "purrrr", "panther", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
